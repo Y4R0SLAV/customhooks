c5e88661f2ae7ca12643bc79da32a710818e47c8
Y4ROSLAV <jeros1337@gmail.com>
2022-09-10 18:21:48 +0300

    useInput hook for controlled inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react'
+import { useInput } from './hooks/useInput'
 
 function App() {
+  const username = useInput("")
+  const password = useInput("")
+
   return (
-    <div className="App">
-      
+    <div>
+      <input {...username} placeholder="Username"/>
+      <input {...password} placeholder="Password"/>
+
+      <button onClick={() => console.log(username.value + " " + password.value)}> Click </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/hooks/useInput.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export const useInput = ( initValue: string) => {
+  const [value, setValue] = useState(initValue) 
+  
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }
+
+  return {
+    value,
+    onChange
+  }
+}
